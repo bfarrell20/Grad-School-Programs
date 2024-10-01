43e9303dac859c6642ec77801b5ba73e51679145
bfarrell20 <118231750+bfarrell20@users.noreply.github.com>
2024-10-01 13:40:27 -0400

    HRI Mini-Study

--- HRI/Mini-Study/RoboticVoice.py
+++ HRI/Mini-Study/RoboticVoice.py
@@ -2,6 +2,11 @@ import time
 from spherov2 import scanner
 from spherov2.sphero_edu import SpheroEduAPI
 from spherov2.types import Color
+from playsound import playsound
 
 toy = scanner.find_toy() ##Change this to the name of my robot for use in class
-# with SpheroEduAPI(toy) as droid:
+with SpheroEduAPI(toy) as droid:
+    droid.set_main_led(Color(255, 57, 66))
+    droid.spin(360, 2)
+    playsound('RobotVoice.m4a')
+
