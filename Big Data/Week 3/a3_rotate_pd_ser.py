db73afa4c14ac9e2d7467197733c09ec9ffe127f
bfarrell20 <118231750+bfarrell20@users.noreply.github.com>
2025-01-29 11:53:43 -0500

    added Big Data a3

---
+++ Big Data/Week 3/a3_rotate_pd_ser.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+# def pd.Series.ɸ(n):
+#     return 1
+
+ser = pd.Series([1,2,3,4,5])
+# print (ser.ɸ(1).to_list())  # prints [5, 1, 2, 3, 4]
+# print (ser.ɸ(2).to_list())  # prints [4, 5, 1, 2, 3]
+# print (ser.ɸ(10).to_list()) # prints [1, 2, 3, 4, 5]
+# print (ser.ɸ().to_list()) # prints [5, 4, 3, 2, 1]
