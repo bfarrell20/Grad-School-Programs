7c607f5993ab2e91c27db50e5ed06c7e1cdcb991
bfarrell20 <118231750+bfarrell20@users.noreply.github.com>
2025-01-31 16:33:35 -0500

    updated Big Data A3

--- Big Data/Week 3/a3_rotate_pd_ser.py
+++ Big Data/Week 3/a3_rotate_pd_ser.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 import pandas as pd
+import numpy as np
 
-# def pd.Series.ɸ(n):
-#     return 1
+def ɸ(self, n=None):
+    if n is None:
+        # Reverse the series if there is no n value given
+        return self[::-1]
+    else:
+        indices = np.arange(len(self))
+        new_indices = indices - n
+        n = new_indices % len(self)
+        shifted_values = self.values.take(n)
+        return pd.Series(shifted_values)
+    
+# Add the function to the pd.Series class
+pd.Series.ɸ = ɸ
 
-ser = pd.Series([1,2,3,4,5])
-# print (ser.ɸ(1).to_list())  # prints [5, 1, 2, 3, 4]
-# print (ser.ɸ(2).to_list())  # prints [4, 5, 1, 2, 3]
-# print (ser.ɸ(10).to_list()) # prints [1, 2, 3, 4, 5]
-# print (ser.ɸ().to_list()) # prints [5, 4, 3, 2, 1]
+# Example usage
+ser = pd.Series([1, 2, 3, 4, 5])
+
+print(ser.ɸ(1).to_list())  # Output: [5, 1, 2, 3, 4]
+print(ser.ɸ(2).to_list())  # Output: [4, 5, 1, 2, 3]
+print(ser.ɸ(10).to_list()) # Output: [1, 2, 3, 4, 5]
+print(ser.ɸ().to_list())   # Output: [5, 4, 3, 2, 1]
